b24151f44ca915020c12726230f686cb7a13e67a
yan <freshjs.oftd@gmail.com>
2022-02-15 21:04:09 +0200

    Test operator AND - OR

--- src/index.js
+++ src/index.js
@@ -1,76 +1,34 @@
+// let num = 10;
+// num = num + 1;
+// // num++;
+// console.log(num++);
+// console.log(num++);
+// console.log(num--);
+// console.log(--num);
 
-// console.log(nullable);
-// console.log(typeof nullable);
+// console.log(2 && 10);
+// console.log(0 || null || NaN);
 
-// let fromHTML = Number(prompt('Enter a number'));
-// console.log(typeof fromHTML);
-// if (typeof nullable === 'string') {
-// 	console.log('It is string');
-// } else if (typeof logic === 'boolean') {
-// 	console.log('It is boolean');
+// let year = Number(prompt('Enter a year'));
+
+// if((year % 100 !== 0 || year % 400 === 0) && year % 4 === 0){
+// console.log('This year is leap year');
 // } else {
-
+//   console.log('This year is not leap year');
 // }
 
-// let result = (caset > 25) ? 17 : (caset === 20) ? 'true' : 'false';
-
-// console.log(result);
-
-
-const sum = function(a, b){
-  return a + b;
+function checkLeapYear() {
+	let year = Number(prompt('Enter a year'));
+	if (isNaN(year)) {
+		console.log("It's wrong year. Be careful");
+	} else {
+		(year % 100 !== 0 || year % 400 === 0) &&
+			year % 4 === 0 &&
+			console.log('This year is leap year');		
+			((year % 100 !== 0 || year % 400 === 0) && year % 4 === 0) ||
+			console.log('This year is not leap year');
+	}
 }
-const sub = function(a, b){
-  return a - b;
-}
-const div = function(a, b){
-  return a / b;
-}
-const mul = function(a, b){
-  return a * b;
-}
+checkLeapYear();
 
-const calculate = function(num1, num2, mathOperation){
-  if(isNaN(num1 - num2)){
-    return 'Enter number, please'
-  }
 
-let operation;
-
-switch (mathOperation){
-  case '+': {
-    operation = sum;
-    break;
-  }
-  case '-': {
-    operation = sub;
-    break;
-  }
-  case '/': {
-    operation = div;
-    break;
-  }
-  case '*': {
-    operation = mul;
-    break;
-  }
-  default: {
-    console.log('Unknown math operation');
-  }
-}
-
-if(typeof operation === 'function'){
-  return operation(num1, num2);
-}else{
-  return 'Unknown math operation';
-}
-
-}
-
-const userInp1 = Number(prompt('Enter first number'));
-const userInp2 = Number(prompt('Enter second number'));
-const mathOperation = prompt('Enter math operation');
-
-const result = calculate(userInp1, userInp2, mathOperation);
-
-console.log(result);
